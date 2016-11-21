663d23b7afbda43cf07377a7cdd89a42f9f5eaf1
hurtsky <hurtskyxda@gmail.com>
2016-11-21 23:10:57 +0800

    import first design
    Signed-off-by: hurtsky <hurtskyxda@gmail.com>

---
+++ content/facebookpage.php
@@ -0,0 +1,6 @@
+
+<div class="fb-page" data-href="https://www.facebook.com/Rojak-RO-958731457605537/" data-tabs="timeline,events,messages" data-height="500" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+	<blockquote cite="https://www.facebook.com/Rojak-RO-958731457605537/" class="fb-xfbml-parse-ignore">
+		<a href="https://www.facebook.com/Rojak-RO-958731457605537/">Rojak RO Facebook Page</a>
+	</blockquote>
+</div>
